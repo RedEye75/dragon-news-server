0cb1d8d29e4d49df98355188f7834837fed2c6ed
RedEye75 <yusufbinemamstk@gmail.com>
2022-11-11 16:34:45 +0600

    news added on the server

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 5000;
 const categories = require("./data/categories.json");
 
 app.use(cors());
-// create api
+
+// dragon api
 app.get("/", (req, res) => {
   res.send("dragon news server running");
 });
